f12b6a48f3d953895271b769d5b042c419774cb9
ayz18 <aris.zhu19@imperial.ac.uk>
2021-06-03 13:33:24 +0100

    add tests for questionStore

---
+++ frontend/drp14Tests/questionStoreTests.swift
@@ -0,0 +1,41 @@
+//
+//  questionStoreTests.swift
+//  drp14Tests
+//
+//  Created by Aris Zhu Yi Qing on 03/06/2021.
+//
+
+import XCTest
+@testable import drp14
+
+class questionStoreTests: XCTestCase {
+	
+	let questionStore = QuestionStore()
+
+	override func setUpWithError() throws {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+	
+	func testInitializeQuestionStoreWithNoQuestionByDefault() throws {
+		XCTAssertEqual(questionStore.questions.count, 0, "There should be no questions when initialized")
+	}
+	
+	func testAddQuestionToQuestionStore() throws {
+		questionStore.add(question: "question A")
+		XCTAssertEqual(questionStore.questions.count, 1, "There should be one question now!")
+	}
+
+	/*
+	func testPerformanceExample() throws {
+		// This is an example of a performance test case.
+		self.measure {
+			// Put the code you want to measure the time of here.
+		}
+	}
+	*/
+
+}
